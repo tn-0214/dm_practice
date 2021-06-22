af0d6dede82dce8eec053be2976af4fe99055e6a
tn-0214 <79251508+tn-0214@users.noreply.github.com>
2021-06-22 19:02:41 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'user/index'
-  get 'user/show'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, :only => [:index, :show]
+  root "users#index"
+  resources :messsages, :only => [:create]
+  resources :rooms, :only => [:create, :show, :index]
 end
